84e6fc26884e36fb2d0536c2801b43b0a3fd8f76
Sabarirepository <97425221+Sabarirepository@users.noreply.github.com>
2025-05-23 17:01:28 -0600

    Update git
    Update git

--- test.py
+++ test.py
@@ -1,1 +1,11 @@
+import os
+
 print("Ran the stage")
+
+AWS_ACCESS_KEY_ID = os.getenv("AWS_ACCESS_KEY_ID")
+SECRET_ACCESS_KEY = os.getenv("SECRET_ACCESS_KEY")
+REGION = os.getenv("REGION")
+
+print("AWS Access Key:", AWS_ACCESS_KEY_ID)
+print("Secret Access Key:", SECRET_ACCESS_KEY)
+print("Region:", REGION)
